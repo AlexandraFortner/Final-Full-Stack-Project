f7560298576241e3a3b7836c03004e2d91fb820b
Alexandra <afortner@basecampcodingacademy.org>
2018-03-09 14:37:48 -0600

    Fixed Spring problem.

--- Backend/src/main/java/com/example/Backend/DemoApplication.java
+++ Backend/src/main/java/com/example/Backend/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example.Backend;
-
+import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello World!");
 	}
 }
