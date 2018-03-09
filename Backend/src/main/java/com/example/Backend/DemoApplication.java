25480cb8c0313be9f8a1f26681505d07e3d1fd08
Alexandra <afortner@basecampcodingacademy.org>
2018-03-09 15:29:37 -0600

    Added sql file.

--- Backend/src/main/java/com/example/Backend/DemoApplication.java
+++ Backend/src/main/java/com/example/Backend/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.Backend;
 import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
