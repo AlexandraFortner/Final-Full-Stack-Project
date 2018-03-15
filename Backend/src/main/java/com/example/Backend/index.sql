00251bfcab57562402222c48a5f4f5915915a9a1
Alexandra <afortner@basecampcodingacademy.org>
2018-03-15 04:48:32 -0500

    Created users table and genre table.
    Users table contains username and password_hash(meant for future encryption). Genres table for use with future story table.

---
+++ Backend/src/main/java/com/example/Backend/index.sql
@@ -0,0 +1,21 @@
+--- ! TABLES !
+
+--    create table users(
+--        id serial unique primary key,
+--        username varchar(150),
+--        password_hash varchar(250)
+--    );
+
+--    create table story(
+--        id serial unique primary key,
+--        story_title varchar(200),
+--        story_date current_date?????,
+--        story text,
+--        genre ????to_be_decided_at_a_later_date????,
+--        stars (or votes) int
+--    );
+
+--    create table genre(
+--    id serial unique primary key,
+--    genre_name varchar(200)
+--    );
