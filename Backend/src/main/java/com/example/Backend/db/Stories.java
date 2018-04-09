3840ee28d26099a6b8a0e93aa6762a4f13d73dd2
Alexandra <afortner@basecampcodingacademy.org>
2018-04-09 10:56:17 -0500

    Ordered story feed.
    Story Feed now has all stories in descending order.
    
    Changed via line 19 Story.java.

--- Backend/src/main/java/com/example/Backend/db/Stories.java
+++ Backend/src/main/java/com/example/Backend/db/Stories.java
@@ -6,32 +6,32 @@ import com.example.Backend.core.Story;
 import com.example.Backend.db.Connect;
 import java.sql.*;
 
-public class Stories {
     public Story story;
 
     public Stories(Story story) {
         this.story = story;
     }
 
-        public static List<Story> all() {
-            try{
-                Connection c = Connect.connect();
-                PreparedStatement st = c.prepareStatement("SELECT * FROM stories;");
-                ResultSet rs = st.executeQuery();
-                ArrayList<Story> allstories = new ArrayList<Story>();
-                while (rs.next()) {
-                    allstories.add(new Story(
-                            rs.getInt("id"),
-                            rs.getString("story_author_name"),
-                            rs.getString("story_title"),
-                            rs.getDate("story_date"),
-                            rs.getString("story"),
-                            rs.getInt("genre_id"),
-                            rs.getString("story_summary")
-                    ));
-                    // ABOVE PARENTHESES() GETS SQL COLUMN NAMES
-            }
-            return allstories;
+public class Stories {
+    public static List<Story> all() {
+        try{
+            Connection c = Connect.connect();
+            PreparedStatement st = c.prepareStatement("SELECT * FROM stories order by story_date desc;");
+            ResultSet rs = st.executeQuery();
+            ArrayList<Story> allstories = new ArrayList<Story>();
+            while (rs.next()) {
+                allstories.add(new Story(
+                        rs.getInt("id"),
+                        rs.getString("story_author_name"),
+                        rs.getString("story_title"),
+                        rs.getDate("story_date"),
+                        rs.getString("story"),
+                        rs.getInt("genre_id"),
+                        rs.getString("story_summary")
+                ));
+                // ABOVE PARENTHESES() GETS SQL COLUMN NAMES
+        }
+          return allstories;
         }
         catch (Exception e) {
             e.printStackTrace();
